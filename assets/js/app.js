5d704beedfb15842c2e9e207c4c1e3686792d960
root <root@project-dev.pensoft.local>
2023-08-24 09:18:15 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,299 @@
+var viewed = false;
+
+var width = $(window).width();
+
+
+$(document).ready(function() {
+	/* MENU */
+	$('.navbar-nav').attr('id', 'menu'); // please don't remove this line
+	$( '<div class="calendar-top"></div>' ).insertBefore( "#calendar" );
+	$( '<div class="card-profile-top"></div>' ).insertBefore( ".card.profile.card-profile" );
+	var divs = $(".card-profiles > div");
+	for(var i = 0; i < divs.length; i+=2) {
+		divs.slice(i, i+2).wrapAll( '<div class="col-xs" />');
+	}
+
+	var headerNavbar = $('#headerNavbar');
+	var width100 = $('.width100');
+	var innerWidth = $('body').innerWidth();
+	headerNavbar.width(innerWidth);
+	width100.width(innerWidth);
+
+    $('.nav-item').children("a").each(function(){
+        if($(this).attr('data-toggle') == 'dropdown'){
+            $(this).removeAttr('data-toggle')
+        }
+    });
+
+    if (width < 992) { // mobile
+        $('#menuToggle input[type="checkbox"]').change(function(){
+            var checked = $(this).is(":checked");
+            if(checked){
+                $('#menu').show("slide", { direction: "right" }, 400);
+                $('#search').hide();
+                $('#menu, #menu *').css({
+                    'visibility': 'visible'
+                });
+                // $('.navbar.fixed').css('position', 'absolute');
+                // $('body', 'html').css({
+                //     'overflow': 'hidden'
+                // });
+            }else{
+                $('#menu').hide("slide", { direction: "right" }, 400);
+                $('#search').hide();
+                $('.navbar.fixed').css('position', 'fixed');
+                // $('.navbar').addClass('fixed');
+                // $('body', 'html').css({
+                //     'overflow': 'auto'
+                // });
+            }
+        });
+    }
+
+    $('.dropdown a').click(function(event) {
+
+        if (location.href.indexOf("#") != -1) {
+            var link = $(this).attr('href');
+            var anchorId = link.substr(link.indexOf("#") + 1);
+            if($("#"+anchorId).length > 0){
+
+                window.location.href = link;
+                location.reload();
+            }
+        }
+    });
+
+
+    $('body').on('click', '.projects_services .accordion-toggle', function () {
+        if ($(this).next(".accordion-content").is(':visible')) {
+            $(this).next(".accordion-content").slideUp(300);
+            $(this).children(".plusminus").html('<span class="plus"></span>');
+        } else {
+            $(this).next(".accordion-content").slideDown(300);
+            $(this).children(".plusminus").html('<span class="minus"></span>');
+        }
+    });
+
+
+
+    $('.projects_services .accordion-content, .projects_services .accordion-toggle').each(function( index, value ) {
+        $(value).find('a').attr( "onclick", "window.open(this.href, '_blank');" )
+    });
+
+    $('.projects_services').removeAttr('role');
+
+	$('.nav.nav-pills').removeAttr('id');
+
+    $('.tabs').each(function(){
+        // For each set of tabs, we want to keep track of
+        // which tab is active and its associated content
+        var $active, $content, $links = $(this).find('a');
+        var speed = "fast";
+        var activeTab = $(location.hash);
+        // If the location.hash matches one of the links, use that as the active tab.
+        // If no match is found, use the first link as the initial active tab.
+        $active = $($links.filter("[href=\'"+location.hash+"\']")[0] || $links[0]);
+
+        // if($(this).parent().parent().hasClass('events')){
+            $active.addClass('active');
+        // }
+
+        $content = $($active[0].hash);
+
+        // Hide the remaining content
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+
+        if(activeTab.length){
+            $content.slideDown(speed);
+            //scroll to element
+            // $('html, body').animate({
+            //     scrollTop:  activeTab.offset().top - $('header').height()*2
+            // }, speed);
+        }
+
+        // Bind the click event handler
+        $(this).find("a").click(function (e) {
+            // if($(this).hasClass('active')) {
+            //     $content.slideDown(speed);
+            //     e.preventDefault();
+            //     return false;
+            // }
+            // // Make the old tab inactive.
+            // $active.removeClass('active');
+            // // $content.slideUp({
+            // // 	scrollTop: $content.offset().top - $('header').height() - $('.left_sidebar').height()
+            // // }, speed);
+            // $content.hide();
+            //
+            // // Update the variables with the new link and content
+            // $active = $(this);
+            // $content = $(this.hash);
+            //
+            // location.hash = $active[0].hash;
+            //
+            // // Make the tab active.
+            // $active.addClass('active');
+            // $content.slideDown(speed);
+
+
+            if($(this).hasClass('active')) {
+                $content.slideDown({
+                    scrollTop: 0
+                }, speed);
+                var screenSize = getScreenSize();
+                if (screenSize.width < 800) {
+                    // scroll to element
+                    $('html, body').animate({
+                        scrollTop: 0  // mobile
+                    }, speed);
+                }else{
+                    //scroll to element icons top
+                    $('html, body').animate({
+                        scrollTop:  0
+                    }, speed);
+                }
+                e.preventDefault();
+                return false;
+            }
+            // Make the old tab inactive.
+            $active.removeClass('active');
+            // $content.slideUp({
+            // 	scrollTop: $content.offset().top - $('header').height() - $('.left_sidebar').height()
+            // }, speed);
+            $content.hide();
+
+            // Update the variables with the new link and content
+            $active = $(this);
+            $content = $(this.hash);
+
+            location.hash = $active[0].hash;
+
+            // Make the tab active.
+            $active.addClass('active');
+            if($content.offset()){
+                $content.slideDown({
+                    scrollTop: 0
+                }, speed);
+            }
+
+            // Prevent the anchor\'s default click action
+            e.preventDefault();
+        });
+    });
+
+	$('.about h1.display-1').attr('data-aos', 'fade-up');
+	$('h2.underline').attr('data-aos', 'fade-up');
+
+});
+
+
+// $('.tabs').each(function(){
+//     // For each set of tabs, we want to keep track of
+//     // which tab is active and its associated content
+//     var $active, $content, $links = $(this).find('a');
+//     var speed = "fast";
+//     var activeTab = $(location.hash);
+//     // If the location.hash matches one of the links, use that as the active tab.
+//     // If no match is found, use the first link as the initial active tab.
+//     $active = $($links.filter("[href=\'"+location.hash+"\']")[0] || $links[0]);
+//
+//     if($(this).parent().parent().hasClass('events')){
+//         $active.addClass('active');
+//     }
+//
+//     $content = $($active[0].hash);
+//
+//     // Hide the remaining content
+//     $links.not($active).each(function () {
+//         $(this.hash).hide();
+//     });
+//
+//     if(activeTab.length){
+//         $content.slideDown(speed);
+//         //scroll to element
+//         $('html, body').animate({
+//             scrollTop:  0
+//         }, speed);
+//     }
+//
+//     // Bind the click event handler
+//     $(this).find("a").click(function (e) {
+//         if($(this).hasClass('active')) {
+//             $content.slideDown({
+//                 scrollTop: 0
+//             }, speed);
+//             var screenSize = getScreenSize();
+//             if (screenSize.width < 800) {
+//                 // scroll to element
+//                 $('html, body').animate({
+//                     scrollTop: 0  // mobile
+//                 }, speed);
+//             }else{
+//                 //scroll to element icons top
+//                 $('html, body').animate({
+//                     scrollTop:  0
+//                 }, speed);
+//             }
+//             e.preventDefault();
+//             return false;
+//         }
+//         // Make the old tab inactive.
+//         $active.removeClass('active');
+//         // $content.slideUp({
+//         // 	scrollTop: $content.offset().top - $('header').height() - $('.left_sidebar').height()
+//         // }, speed);
+//         $content.hide();
+//
+//         // Update the variables with the new link and content
+//         $active = $(this);
+//         $content = $(this.hash);
+//
+//         location.hash = $active[0].hash;
+//
+//         // Make the tab active.
+//         $active.addClass('active');
+//         if($content.offset()){
+//             $content.slideDown({
+//                 scrollTop: 0
+//             }, speed);
+//         }
+//
+//
+//         // Prevent the anchor\'s default click action
+//         e.preventDefault();
+//     });
+// });
+
+function GoToPage(page){
+    window.open('/'+page, '_self');
+}
+
+
+function redirectAndRefresh(url){
+	$(".tabs a").each(function() {
+		this.href = window.location.hash;
+	});
+	window.open(url, '_blank');
+	location.reload();
+}
+
+function isBreakpointLarge() {
+    return window.innerWidth <= 991;
+}
+
+function showSearchForm(){
+	$('#layout-header').toggleClass('full-width');
+	$('#search').toggle();
+	$('#menu li').hide();
+	$('nav a:not(.navbar-brand)').hide();
+}
+
+function hideSearchForm(){
+	$('#layout-header').toggleClass('full-width');
+	$('#search').hide();
+	$('#menu li').show();
+    $('nav a').show();
+}
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'october-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ea41c8589fc6fe5f49c144a0a6f63dccfa59a82e',
+        'name' => 'october/pensoft-forestpaths-theme',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'october/pensoft-forestpaths-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'october-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ea41c8589fc6fe5f49c144a0a6f63dccfa59a82e',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
